7a0386458dd4a51cdb84712384e7efd30ed5fcb3
Flightstar <haunt.hcm2015@gmail.com>
2018-03-02 01:25:15 +0700

    Set up server and angular 5

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
+const cors = require('cors');
+
+const app = express();
+mongoose.connect(config.database, err => {
+    if (err)
+        console.log(err);
+    else
+        console.log("Connected to the database");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+    res.json({
+        "user" : "Hau Trung Nguyen"
+    });
+})
+app.listen(config.port, err => {
+
+    console.log('Magic happen on port 3030');
+});
