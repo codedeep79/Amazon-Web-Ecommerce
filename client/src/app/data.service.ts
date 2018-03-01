bdefc61f20d7f4d7475f5f299a8e4b7bd483c17d
Flightstar <haunt.hcm2015@gmail.com>
2018-03-02 01:41:25 +0700

    set up server and angular 5

---
+++ client/src/app/data.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { RestApiService } from './rest-api.service';
+import { NavigationStart, Router } from '@angular/router';
+
+
+@Injectable()
+export class DataService {
+  message = "";
+  messageType = "danger";
+
+  constructor(private router: Router, private rest: RestApiService) {
+    this.router.events.subscribe(event => {
+      
+      if (event instanceof NavigationStart) {
+        this.message = "";
+      }
+
+    });
+
+   }
+
+   error(message) {
+     this.message = message;
+     this.messageType = "danger";
+   }
+
+   success(message) {
+     this.message = message;
+     this.messageType = "success";
+   }
+
+   warning(message) {
+    this.message = message;
+    this.messageType = "warning";
+   }
+
+}
